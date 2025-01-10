1e036b94538adb12610531dd5fb098b16c3acd3c
Priyanshi dhakad <prayanshudhakad405@gmail.com>
2025-01-10 09:26:06 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+function createStars(){
+    const container=document.querySelector("body");
+    for(let i=0; i<1000; i++){
+const star=document.querySelector("div");
+// star.className="star";
+// star.style.width=".1px";
+// star.style.height=".1px";
+star.style.top=Math.random()*100*"%"
+star.style.left=Math.random()*100*"%"
+    }
+}
